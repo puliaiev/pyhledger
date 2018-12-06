953ba49f39de9338f71fb2637705fd6618b09eed
Sergey Puliaiev <sergey.puliaiev@collabio.team>
2018-12-06 20:40:30 +0100

    rename package

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='pyhledger',
-      version='0.1',
+setup(name='ledger',
+      version='0.2',
       description='',
       url='https://github.com/btittelbach/pyhledger/',
       author='',
       author_email='',
       license='',
-      packages=['pyhledger'],
+      packages=['ledger'],
       zip_safe=False)
